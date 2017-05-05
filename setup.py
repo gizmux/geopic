c93af34f3e4aa9976079897c9a1f5c30aee7ed87
Vasileios Gkiziakis <gizmux@gmail.com>
2017-05-05 21:35:26 +0100

    package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geopic',
+      version='0.1',
+      description='Simple extraction of geographic information from exif data',
+      url='http://github.com/gizmux/geopic',
+      author='gizmux',
+      author_email='gizmux@gmail.com',
+      license='MIT',
+      packages=['geopic'],
+      zip_safe=False)
