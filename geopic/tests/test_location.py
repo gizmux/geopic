ff120b850a49566095238ef46864a12127883f91
Vasileios Gkiziakis <gizmux@gmail.com>
2017-05-06 20:21:54 +0100

    Indent using spaces in all code

--- geopic/tests/test_location.py
+++ geopic/tests/test_location.py
@@ -4,10 +4,10 @@ import geopic
 
 class TestLocation(TestCase):
 
-	def test_invalid_coordinates(self):
-		loc = geopic.Location(99, 99999)
-		self.assertFalse(loc.isValid())
+    def test_invalid_coordinates(self):
+        loc = geopic.Location(99, 99999)
+        self.assertFalse(loc.isValid())
 
-	def test_country(self):
-		loc = geopic.Location(37.9709961, 23.6916685)
-		self.assertTrue(loc.country() == "Greece")
+    def test_country(self):
+        loc = geopic.Location(37.9709961, 23.6916685)
+        self.assertTrue(loc.country() == "Greece")
