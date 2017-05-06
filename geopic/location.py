f58231c11c18513d8bd6f679db28d00df3054c5b
Vasileios Gkiziakis <gizmux@gmail.com>
2017-05-06 22:17:45 +0100

    Added zones support in Location class

--- geopic/location.py
+++ geopic/location.py
@@ -1,4 +1,5 @@
 import logging
+from operator import itemgetter
 
 from pygeocoder import Geocoder, GeocoderError
 
@@ -11,12 +12,13 @@ class Location:
         self._country   = None
         self._zones     = None
         try:
-            self.geocoder_result = Geocoder.reverse_geocode(self.lat, self.lng)
+            self.geocoder_results = Geocoder.reverse_geocode(self.lat, self.lng)
         except GeocoderError as ge:
-            logging.error("reverse_geocode for ({0},{1}) failed: '{2}'".format(self.lat, self.lng, ge))
+            logging.error("reverse_geocode for ({0},{1}) failed: '{2}'".format(
+                self.lat, self.lng, ge))
             #TODO: CONSIDER: is this the best strategy here ?
             return
-        if self.geocoder_result.count > 0:
+        if self.geocoder_results.count > 0:
             self.valid = True
 
     def isValid(self):
@@ -26,8 +28,49 @@ class Location:
         """
         Return the country of a valid set of coordinates
         """
+        if self._country is not None:
+            return self._country
         if not self.isValid():
             return None
-        if self._country is None:
-            self._country = self.geocoder_result.country
+        self._country = self.geocoder_results.country
         return self._country
+
+    def zones(self, limit=5):
+        """
+        Return a list of the Location zones from most general to most specific.
+        Limit the depth of the search for zones to 'limit'
+        """
+        if self._zones is not None:
+            return self._zones
+        if not self.isValid():
+            return None
+        if not type(limit) is int:
+            return None
+
+        # First we extract all available zones up to the specified limit from
+        # the reverse_geocode results
+        temp_zones = dict()
+        for datum in self.geocoder_results.data:
+            for component in datum["address_components"]:
+                for level in range(1, limit + 1):
+                    zone_variable = "zone" + str(level)
+                    admin_area_variable = ("administrative_area_level_" +
+                        str(level))
+                    if admin_area_variable in component["types"]:
+                        admin_area = component["long_name"]
+                        if zone_variable not in temp_zones:
+                            temp_zones[zone_variable] = admin_area
+                        elif temp_zones[zone_variable] != admin_area:
+                            #TODO: perhaps raise an exception here ?
+                            logging.warning(("'{0}' is inconsistent within the "
+                                "reverse_geocode results: "
+                                "'{1}' != '{2}'").format( admin_area_variable,
+                                    temp_zones[zone_variable], admin_area))
+                        continue
+        # reset the results data object
+        self.geocoder_results.current_index = 0
+
+        # Now we create the list in general -> specific order
+        self._zones = [ x for (k, x) in sorted(temp_zones.iteritems()) ]
+
+        return self._zones
--- geopic/tests/test_location.py
+++ geopic/tests/test_location.py
@@ -11,3 +11,7 @@ class TestLocation(TestCase):
     def test_country(self):
         loc = geopic.Location(37.9709961, 23.6916685)
         self.assertTrue(loc.country() == "Greece")
+
+    def test_zones(self):
+        loc = geopic.Location(35.1418955, 24.5955356)
+        self.assertTrue(loc.zones() == ["Crete Region", "Crete", "Rethimno", "Agios Vasileios", "Lampi"])
