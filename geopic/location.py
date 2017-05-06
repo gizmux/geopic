f61fd30e2ac94b004849b76fb0661fd717e7669c
Vasileios Gkiziakis <gizmux@gmail.com>
2017-05-06 20:42:58 +0100

    pull useful data into the Location object on demand

--- geopic/location.py
+++ geopic/location.py
@@ -5,17 +5,17 @@ from pygeocoder import Geocoder, GeocoderError
 class Location:
 
     def __init__(self, lat, lng):
-        self.lat = lat
-        self.lng = lng
-        self.valid = False
-
+        self.lat        = lat
+        self.lng        = lng
+        self.valid      = False
+        self._country   = None
+        self._zones     = None
         try:
             self.geocoder_result = Geocoder.reverse_geocode(self.lat, self.lng)
         except GeocoderError as ge:
             logging.error("reverse_geocode for ({0},{1}) failed: '{2}'".format(self.lat, self.lng, ge))
             #TODO: CONSIDER: is this the best strategy here ?
             return
-
         if self.geocoder_result.count > 0:
             self.valid = True
 
@@ -28,4 +28,6 @@ class Location:
         """
         if not self.isValid():
             return None
-        return self.geocoder_result.country
+        if self._country is None:
+            self._country = self.geocoder_result.country
+        return self._country
